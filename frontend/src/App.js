4f7ed667730894fb849d6eda65a4fd3581913945
an.lq295.work <an.lq295.work@gmail.com>
2025-12-21 11:52:07 +0700

    1

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -44,8 +44,10 @@ function AppRoutes() {
       <Route path="/" element={<MainLayout />}>
         {/* Mặc định hiện Map */}
         <Route index element={<LeafletMapComponent />} />
-        {/* Route cho trang gợi ý món ăn */}
-        <Route path="/recommendations" element={<DishRecommendationPage />} />
+
+        {/* 👇 Thêm Route cho trang Gợi ý món ăn */}
+        <Route path="recommendations" element={<DishRecommendationPage />} />
+
         {/* Route cho trang danh sách địa điểm gần đây */}
         <Route path="nearby" element={<LocationListPage />} />
         {/* Route cho trang danh sách địa điểm yêu thích */}
